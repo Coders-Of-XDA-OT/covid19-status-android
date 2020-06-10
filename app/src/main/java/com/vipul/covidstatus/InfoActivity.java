ae56ba94042051050722cc83e22e2ac00f4c7967
Vipul Jha <vipuljha08@gmail.com>
2020-06-10 15:12:24 +0530

    InfoActivity: Check for updates button added
    Signed-off-by: Vipul Jha <vipuljha08@gmail.com>

--- app/src/main/java/com/vipul/covidstatus/InfoActivity.java
+++ app/src/main/java/com/vipul/covidstatus/InfoActivity.java
@@ -1,18 +1,35 @@
 package com.vipul.covidstatus;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class InfoActivity extends AppCompatActivity {
+
+    String version;
+    String updateUrl;
+    String updateChanges;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,5 +63,69 @@ public class InfoActivity extends AppCompatActivity {
 
     public void openTwitter(View view) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.twitter.com/lordarcadius")));
+    }
+
+    public void checkUpdate(View view){
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String apiUrl = "https://vipul-api.netlify.app/api/version.json";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, apiUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    version = response.getString("version");
+                    updateUrl = response.getString("url");
+                    updateChanges = response.getString("changes");
+
+                    if (!version.equals(String.valueOf(BuildConfig.VERSION_NAME))){
+                        new AlertDialog.Builder(InfoActivity.this)
+                                .setTitle("v"+version+" update available!")
+                                .setMessage(updateChanges)
+                                .setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                                        intent.setData(Uri.parse(updateUrl));
+                                        startActivity(intent);
+                                    }
+                                })
+                                .setNegativeButton("Ignore", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create().show();
+                    } else {
+                        Toast.makeText(InfoActivity.this, "No update available!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    public void showChanges(View view){
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.changelogTitle)
+                .setMessage(R.string.changelog)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setCancelable(false)
+                .create().show();
     }
 }
